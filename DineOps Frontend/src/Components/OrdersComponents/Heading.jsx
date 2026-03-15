8091d0b9b8550415c510e328e3d29b914b00e551
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-03-15 19:06:43 +0530

    Completed with header

--- DineOps Frontend/src/Components/OrdersComponents/Heading.jsx
+++ DineOps Frontend/src/Components/OrdersComponents/Heading.jsx
@@ -4,6 +4,10 @@ const Heading = () => {
   return (
     <div>
       <button>{/* Add circular logo of Back button*/}</button>
+      <button>All</button>
+      <button>inprogess</button>
+      <button>ready</button>
+      <button>Completed</button>
     </div>
   )
 }
