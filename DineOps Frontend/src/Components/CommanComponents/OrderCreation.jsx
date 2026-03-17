3e394c70d3df2827091bae97631f7ea946a2cfa6
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-03-17 21:48:18 +0530

    initiated

--- DineOps Frontend/src/Components/CommanComponents/OrderCreation.jsx
+++ DineOps Frontend/src/Components/CommanComponents/OrderCreation.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const OrderCreation = () => {
+const OrderCreation = ({title,isOpen,isClose,children}) => {
+    if(!isOpen)return null;
   return (
     <div>
-      
+      <div>
+        <h1>create order</h1>
+        <button>{/* add cut logo  */}</button>
+      </div>
+            
+      <div>
+
+      </div>
     </div>
   )
 }
