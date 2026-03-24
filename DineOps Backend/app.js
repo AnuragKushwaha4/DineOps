4e6a134a07f3a565e505a58f7080cf9b0aa24792
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-03-24 22:16:26 +0530

    Started and setup Backend

---
+++ DineOps Backend/app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express")
+
+
+
+
+const app = express()
+
+
+const PORT = process.env.PORT;
+
+app.get("/",(req,res)=>{
+    res.json({message:"server stared"})
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Started Server : 8000")
+})
