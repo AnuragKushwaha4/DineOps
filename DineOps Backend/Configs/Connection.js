c8a0d259fb9825d63a280b425cd76df41005c8a0
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-03-27 23:03:50 +0530

    DB Connected

--- DineOps Backend/Configs/Connection.js
+++ DineOps Backend/Configs/Connection.js
@@ -6,6 +6,8 @@ async function ConnectDB(){
     try{
         const connections = await mongoose.connect(process.env.MONGODB_URL)
         console.log(`MongoDB connected : ${connections.connection.host}`);
+
+        
     }
     catch{
         console.log(`Error : ${Error}`);
