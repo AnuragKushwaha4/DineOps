f9b50acb0f9f30675981bb5288deae64a816ee48
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-03-29 21:05:50 +0530

    Completed with Schema creation

--- DineOps Backend/Models/UserModel.js
+++ DineOps Backend/Models/UserModel.js
@@ -1,5 +1,30 @@
 const mongoose = require("mongoose")
 
-const userSchema = mongoose.Schema({
-    
-})
+const userSchema =new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        validate: [validator.isEmail, "Please enter a valid email"],
+    },
+    phone:{
+        type:Number,
+        required:true,
+        validate: {
+    validator: function(v) {
+      return validator.isMobilePhone(v, "en-IN");
+    },
+    message: "Please enter a valid Indian phone number"
+  }
+    },
+    role:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+
+
+module.exports = mongoose.Model("User",userSchema)
