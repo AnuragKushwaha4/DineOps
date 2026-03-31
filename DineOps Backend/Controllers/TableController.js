663327b0bd37559e12d1357162a4631db4458b2c
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-04-01 00:03:54 +0530

    Completed with table comtroller

--- DineOps Backend/Controllers/TableController.js
+++ DineOps Backend/Controllers/TableController.js
@@ -1,7 +1,21 @@
+const createHttpError = require("http-errors");
+const TableModel = require("../Models/TableModel");
 
 
 async function AddTable(req,res,next) {
     try{
+
+        const {tableNo}= req.body;
+
+        if(!tableNo)return next(createHttpError(400,"Table number is not provided"))
+
+        const isTablePresent = await TableModel.findOne({tableNo:tableNo})
+
+        if(isTablePresent)return next(createHttpError(400,"Table Already Exist"))
+
+        const NewTable = await TableModel.create({tableNo:tableNo})
+
+        res.status(200).json({success:true,message:"New Table created",data:NewTable})
 
     }catch(error){
         next(error)
@@ -11,6 +25,8 @@ async function AddTable(req,res,next) {
 async function GetTables(req,res,next) {
     try{
 
+        const Tables = await TableModel.findOne();
+        res.status(200).status({success:true,message:"All the Tables sent",data:Tables})
     }catch(error){
         next(error)
     }
@@ -20,6 +36,20 @@ async function GetTables(req,res,next) {
 async function UpdateTable(req,res,next) {
     try{
 
+        const {tableStatus ,orderID}=req.body;
+        if(!tableStatus || !orderID)return next(createHttpError(400,"All fields required"))
+
+        const Table = await TableModel.findByIdAndUpdate(
+            req.params.id,
+            {tableStatus, orderDetails:orderID },
+            {new:true}
+        )
+
+        if(Table)return next(createHttpError(400,"Table not found"))
+
+        res.status(200).json({success:true,message:"Table updated",data:Table})
+
+        
     }catch(error){
         next(error)
     }
--- DineOps Backend/Models/TableModel.js
+++ DineOps Backend/Models/TableModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const TableSchema = new mongoose.Schema({
     tableNo:{type:Number, required:true, unique:true},
-    tableStatus:{type:String,required:true},
+    tableStatus:{type:String,default:"Available"},
     orderDetails:{type:mongoose.Schema.Types.ObjectId,ref:"Orders"}
 
 })
