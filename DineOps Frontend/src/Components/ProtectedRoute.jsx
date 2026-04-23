2bf5555005542e7b6f435182cc16017b4a3c4150
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-04-23 17:01:08 +0530

    updation

--- DineOps Frontend/src/Components/ProtectedRoute.jsx
+++ DineOps Frontend/src/Components/ProtectedRoute.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 import Loader from "../Components/CommanComponents/Loader"
+
+import { getData } from '../Https/index'
 const ProtectedRoute = ({children}) => {
 const { isAuth } = useSelector((state) => state.user)
-
+const dispatch = useDispatch();
   
 
     if (!isAuth) {
   return <Navigate to="/auth/login" replace />
 }
+
   return (
     children
   )
