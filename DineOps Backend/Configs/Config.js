8b93113cd030b18f7a0bfb88989a69e1f8ec2ee7
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-04-28 22:32:33 +0530

    Completed with the API creation for orderCreate

--- DineOps Backend/Configs/Config.js
+++ DineOps Backend/Configs/Config.js
@@ -5,8 +5,9 @@ const config = Object.freeze({
     port : process.env.PORT || 3000,
     DBURL :process.env.MONGODB_URL,
     nodeEnv : process.env.NODE_ENV || "developement",
-    accessTokenSecret:process.env.JWT_SECRET
-
+    accessTokenSecret:process.env.JWT_SECRET,
+    testapikey : process.env.TEST_API_KEY,
+    testsecretkey : process.env.TEST_KEY_SECRET
 })
 
 module.exports = config
--- DineOps Backend/Controllers/PaymentController.js
+++ DineOps Backend/Controllers/PaymentController.js
@@ -1,8 +1,24 @@
-async function createOrder(){
+const Razorpay = require("razorpay")
+const config = require("../Configs/Config")
+async function createOrder(req,res,next){
     try{
+        var instance = new Razorpay({ key_id: config.testapikey , key_secret: config.testsecretkey })
+
+        const {amount} = req.body;
+
+        var options = {
+            amount: amount*100,  
+            currency: "INR",
+            receipt: `receipt_${Date.now()}`
+        };
+
+        const order = await instance.orders.create(options);
+
+        res.status(200).json({success:true,order})
+
 
     }catch(error){
-
+        next(error)
     }
 }
 
