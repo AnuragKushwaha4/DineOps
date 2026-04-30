efc631607fe3cc7d592e593c0c12b89ddf81216f
AnuragKushwaha4 <anuragjnv4@gmail.com>
2026-04-30 21:46:13 +0530

    Completed with the model creation

--- DineOps Backend/Models/PaymentModel.js
+++ DineOps Backend/Models/PaymentModel.js
@@ -1,1 +1,15 @@
 const mongoose = require("mongoose")
+
+const PaymentSchema =new  mongoose.Schema({
+    paymentId:String,
+    orderId:String,
+    amount:Number,
+    currency:String,
+    status:String,
+    method:String,
+    email:String,
+    contact:String,
+    createdAt:Date
+})
+
+module.exports = mongoose.model("Payment",PaymentSchema)
